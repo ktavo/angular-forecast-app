b0b6ceeaa056392999ecde1af058cd3380fc1655
Tavo <gariosp@unal.edu.co>
2017-08-24 19:47:08 -0300

    Cleaning code

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,6 @@
 app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
 	forecast.then(function(data)
 	{
-		console.log('Inside MainController');
 		$scope.fiveDay = data;
-		$scope.test = "prueba";
-		console.log(JSON.stringify($scope.fiveDay));
 	});
 }]);
